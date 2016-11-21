26130da35cb7d7d4cd7031a4f47a211a3ab3e52c
aluno <aluno@LabX-OptiPlex-7010>
2016-11-21 10:29:07 -0200

    Ativação de código X

---
+++ src/aula/Aula.java
@@ -0,0 +1,18 @@
+package aula;
+public class Aula {
+    public static void main(String[] args) {
+        
+            boolean novaV = false;
+            
+            int inteira = 2;
+            
+            if (inteira <1) {
+                novaV = true;
+                    
+                    
+            }
+            System.out.println("Variavel: "+novaV);
+
+    }
+    
+}
